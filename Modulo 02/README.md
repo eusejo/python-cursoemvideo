8a8c7bea20bd134e3317138f4419492af2dfd34c
euyag <clancysla@gmail.com>
2021-06-24 19:32:04 -0300

    aula finalizada

---
+++ Modulo 02/README.md
@@ -0,0 +1,4 @@
+# **Modulo 02:** Estruturas de Controle
+
+## **Lista das Aulas**
+- Aula012: Condiçôes aninhadas
---
+++ Modulo 02/aulas/aula012.py
@@ -0,0 +1,9 @@
+nome = str(input('qual o seu nome: '))
+
+if nome == 'gustavo':
+    print('nome bunito ein')
+elif nome == 'lucas':
+    print('onti foi massa')
+else:
+    print('q nome ruim ó')
+print(f'é um praser te conhecer {nome}')
