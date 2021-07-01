debf1c4d8fd0abff19d6692569f2d96bcd00f3c4
euyag <clancysla@gmail.com>
2021-07-01 14:59:11 -0300

    uuu

--- Modulo 02/exercicios/d053.py
+++ Modulo 02/exercicios/d053.py
@@ -1,6 +1,6 @@
 print('===== DESAFIO 053 =====')
 
-nome = str(input('digite uma palavra: ')).strip()
+nome = str(input('digite uma palavra: ')).strip().upper()
 
 for c in range(0, len(nome)):
     print(nome[c])
