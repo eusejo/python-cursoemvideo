ab96ef538aca9291217c4a250e0ebe5d2f08d47d
euyag <clancysla@gmail.com>
2021-07-01 14:54:41 -0300

    ex052 terminado

--- Modulo 02/exercicios/d052.py
+++ Modulo 02/exercicios/d052.py
@@ -1,4 +1,18 @@
 print('===== DESAFIO 052 =====')
 
 num = int(input('digite um numero: '))
+tot = 0
 
+for c in range(1, num + 1):
+    if num % c == 0:
+        tot += 1
+        print('\033[33m',end='')
+    else:
+        print('\033[31m',end='')
+    print(f'{c} ', end='')
+print(f'\n\033[mo numero {num} foi divido {tot} vez')
+
+if tot == 2:
+    print(f'por isso o numero {num} é primo')
+else:
+    print('por isso ele não é primo')
