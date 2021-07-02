a1a52760724b4a22b707933b9203a456784e2009
euyag <clancysla@gmail.com>
2021-07-02 14:36:37 -0300

    mais exercicios feitos

--- Modulo 02/exercicios/d053.py
+++ Modulo 02/exercicios/d053.py
@@ -1,6 +1,15 @@
 print('===== DESAFIO 053 =====')
 
 nome = str(input('digite uma palavra: ')).strip().upper()
+palavras = nome.split()
+junto = ''.join(palavras)
+inverso = ''
 
-for c in range(0, len(nome)):
-    print(nome[c])
+for letra in range(len(junto) - 1,- 1, -1):
+    inverso += junto[letra]
+print(f'o inverso de {junto} é {inverso}')
+
+if inverso == junto:
+    print('a frase digita é um palindromo')
+else:
+    print('a frase digitada não é um palindromo')
--- Modulo 02/exercicios/d054.py
+++ Modulo 02/exercicios/d054.py
@@ -1,2 +1,10 @@
 print('===== DESAFIO 054 =====')
 
+tot = 0
+
+for c in range(1, 8):
+    nascimento = int(input(f'em que ano a {c} pessoa nasceu: '))
+    if(2021 - nascimento >= 21):
+        tot += 1
+print(f'{tot} pessoas são maiores de idade')
+print(f'{7 - tot} pessoa ainda não são maior de idade')
